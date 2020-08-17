06657a6d29b572381cb4b0cfa881a4ba48dc3b94
akimzzy <a.akeem@yahoo.com>
2020-08-17 12:46:24 +0100

    Updates

---
+++ precache-manifest.58e3975799f0cd39f7e4e2446f4322a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e83d5de6e2c2b40efb52f66600523689",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a23101636df62272f963",
+    "url": "/static/css/main.21a8b222.chunk.css"
+  },
+  {
+    "revision": "b7cd2efebb7293616a8b",
+    "url": "/static/js/2.d34d899e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d34d899e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a23101636df62272f963",
+    "url": "/static/js/main.cadf54d8.chunk.js"
+  },
+  {
+    "revision": "4a42fdcbdfd3cf55fea3",
+    "url": "/static/js/runtime-main.1c47a842.js"
+  }
+]);
